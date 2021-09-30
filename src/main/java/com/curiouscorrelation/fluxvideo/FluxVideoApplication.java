1d2155ed25e1673b6d0e35b1fa9afb4bc9961c6b
CuriousCorrelation <CuriousCorrelation@protonmail.com>
2021-09-30 14:33:03 +0530

    Initial commit

---
+++ src/main/java/com/curiouscorrelation/fluxvideo/FluxVideoApplication.java
@@ -0,0 +1,13 @@
+package com.curiouscorrelation.fluxvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curiouscorrelation/fluxvideo/FluxVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curiouscorrelation.fluxvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
